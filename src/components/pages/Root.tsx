a658ffb323390ae679193b1cb5c9294edba6f80d
Junyu Wei <weijunyu@users.noreply.github.com>
2025-07-16 11:48:04 +0800

    Styling nav header

--- src/components/pages/Root.tsx
+++ src/components/pages/Root.tsx
@@ -4,9 +4,9 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export function Root() {
   return (
     <>
-      <header className="bg-slate-100">
-        <nav className="px-8 py-3 flex gap-2 max-w-[800px] mx-auto">
-          <Link to="/">Home</Link>
+      <header className="bg-slate-200">
+        <nav className="px-8 py-3 flex gap-2 max-w-[800px] mx-auto font-semibold font-mono">
+          <Link to="/">see</Link>
         </nav>
       </header>
 
